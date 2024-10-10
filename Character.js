a67bbd05f691656db7fbc80c26fc7e4ee1f15e17
ArianFrutoss <arian.frutos@ikasle.aeg.eus>
2024-10-10 13:14:13 +0200

    ADD - create the character class

---
+++ Character.js
@@ -0,0 +1,16 @@
+export default class Character{
+
+    constructor (fullName, health, magick, stamina, potions){
+
+        this.fullName = fullName;
+        this.health = health;
+        this.magick = magick;
+        this.stamina = stamina;
+        this.potions = potions;
+    }
+
+    static from(playerData, potions){
+
+        
+    }
+}
